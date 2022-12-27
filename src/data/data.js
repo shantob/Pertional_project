4b38c68ff2d026659d53c8312a20b6c2f063bc7f
Shanto Bepary <shantobepary575@gmail.com>
2022-12-27 17:18:05 +0600

    update something

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,41 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+import WorkIm from '../assets/projects/work1.jpeg'
+import RealEst from '../assets/projects/work2.jpg'
+import RealEst3 from '../assets/projects/work3.jpg'
+import RealEst4 from '../assets/projects/work4.jpg'
 import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+import video3 from '../assets/video3.mp4'
 export const data=[
-    {
-        id:1,
-        name:"PHP E-COMMERCE APPLICATION",
-        image:WorkIm,
-        github:"https://github.com/shantob/E-comPHP-PDO",
-        live:video1,
-    },
     {
         id:2,
         name:"Laravel E-Commerce Application",
-        image:RealEst,
+        image:WorkIm,
         github:"https://github.com/shantob/Final_Project",
-        live:"",
+        live:video1,
     },
+    {
+        id:1,
+        name:"PHP E-COMMERCE APPLICATION",
+        image:RealEst,
+        github:"https://github.com/shantob/E-comPHP-PDO",
+        live:video2,
+    },
+    {
+        id:1,
+        name:"NEWS BLOG WEBSITE",
+        image:RealEst3,
+        github:"https://github.com/shantob/first_laravel/tree/master",
+        live:'',
+    },
+   
+    {
+        id:2,
+        name:"PHP E-Commerce Application",
+        image:RealEst4,
+        github:"https://github.com/shantob/E-comPHP-PDO",
+        live:video3,
+    },
+
 
 
 
