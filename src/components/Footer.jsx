5b6337a951db77028e70358237196005aef3c853
Shanto Bepary <shantobepary575@gmail.com>
2022-12-27 17:34:21 +0600

    add something

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white sm:pt-5">
       <div className="md:flex md:justify-between md:items-center sm:px-12  px-4 bg-[#ffffff19] py-7">
         <h1
-          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+          className="lg:text-4xl md:ml-7 text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
          md:w-2/5"
         >
           <span className="text-teal-400">My First webpage is</span> Ready to
